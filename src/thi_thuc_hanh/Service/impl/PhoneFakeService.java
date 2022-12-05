6120a2215f6b14de0e8af18e91d0c5f8e13b0c89
maivanthuan <thuan31102003@gmail.com>
2022-12-05 20:09:31 +0700

    dsad

--- src/thi_thuc_hanh/Service/impl/PhoneFakeService.java
+++ src/thi_thuc_hanh/Service/impl/PhoneFakeService.java
@@ -1,27 +1,27 @@
-package thi_thuc_hanh.Service.impl;
-
-import thi_thuc_hanh.Service.IPhoneFakeService;
-
-public class PhoneFakeService implements IPhoneFakeService {
-
-    @Override
-    public void addPhoneFake() {
-
-    }
-
-    @Override
-    public void removePhoneFake() {
-
-    }
-
-    @Override
-    public void displayPhoneFake() {
-
-    }
-
-    @Override
-    public void SearchPhoneFake() {
-
-    }
-
-}
+//package thi_thuc_hanh.Service.impl;
+//
+//import thi_thuc_hanh.Service.IPhoneFakeService;
+//
+//public class PhoneFakeService implements IPhoneFakeService {
+//
+//    @Override
+//    public void addPhoneFake() {
+//
+//    }
+//
+//    @Override
+//    public void removePhoneFake() {
+//
+//    }
+//
+//    @Override
+//    public void displayPhoneFake() {
+//
+//    }
+//
+//    @Override
+//    public void SearchPhoneFake() {
+//
+//    }
+//
+//}
